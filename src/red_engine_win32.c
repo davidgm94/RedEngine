fb643abf3ecf4a9008991c2e6a6bf9022c3ba23b
David González Martín <davidgonzalezmartin94@gmail.com>
2019-10-26 16:11:26 +0200

    Added test preprocessor macros to switch between default program and test functions

--- src/red_engine_win32.c
+++ src/red_engine_win32.c
@@ -4,19 +4,22 @@
 #define WINDOW_TITLE "Red Engine"
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 576
+#define TEST 0
 
 const u32 initialWidth = WINDOW_WIDTH;
 const u32 initialHeight = WINDOW_HEIGHT;
 
+#if TEST == 0
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, INT nCmdShow)
+	PSTR lpCmdLine, INT nCmdShow)
 {
 	winmain_parameters winmainParameters = { .instance = hInstance, . previousInstance = hPrevInstance, .commandLineArguments = lpCmdLine, .showCommand = nCmdShow };
 	engine_window initialResolution = { initialWidth, initialHeight };
 	win32_startup(&winmainParameters, WINDOW_TITLE, &initialResolution, &win32);
 	platform_dependencies win32_vulkan_dependencies = { .windowHandle = win32.handles.window, .anotherOSHandle = win32.handles.winmainArguments.instance };
 
-    vulkan_renderer vk;
+	vulkan_renderer vk;
 	QPC(startVk);
 	vk_load(&vk, &win32_vulkan_dependencies);
 	QPC(endVk);
@@ -28,6 +31,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		platform_HandleEvents(win32.handles.window);
 	}
 
-    vk_destroy(&vk);
-    return 0;
+	vk_destroy(&vk);
+	return 0;
 }
+
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR lpCmdLine, INT nCmdShow)
+{
+
+}
+#endif
